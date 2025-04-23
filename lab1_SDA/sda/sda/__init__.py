ca59bc18c6a72ba45d35a473fbceaa5f295fd79d
caoye <caoye541@gmail.com>
2025-04-23 11:45:33 +0800

    rm SDA

--- lab1_SDA/sda/sda/__init__.py
+++
@@ -1,6 +0,0 @@
-r"""Score-based Data Assimilation"""
-
-from . import mcs
-from . import nn
-from . import score
-from . import utils
--- lab1_SDA/sda/setup.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-import setuptools
-
-setuptools.setup(
-    name='sda',
-    version='1.0.0',
-    packages=setuptools.find_packages(),
-)
